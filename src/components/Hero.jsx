98e01107e681110cd2135c46e146f1322b6e28d9
Samrat Jha <samratj@amazon.com>
2023-03-13 00:27:29 -0400

    Finish Hero section with navigator and mobile friendly

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,11 +13,18 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Samrat</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidde" />interfaces and web application
+            Full stack software engineer dabbling <br className="sm:block hidden" />in 3D visuals and web applications
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev animate={{y: [0, 24,0] }} transition={{duration: 1.5, repeat: Infinity, repeatType: 'loop' }} className="w-3 h-3 rounded-full bg-secondary mb-1"/>
+          </div>
+          </a>
+      </div>
     </section>
   )
 }
