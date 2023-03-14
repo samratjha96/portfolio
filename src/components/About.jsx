0c4b1be66e989bae9a7382b20b3205e874af7160
Samrat Jha <samratj@amazon.com>
2023-03-13 23:27:31 -0400

    Finish Experience section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,9 +24,9 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionS
+        <p className={styles.sectionSubText
         }>Introduction</p>
-        <p className={styles.sectionHeadText}>Overview.</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
       <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 secondary-text-[17px] max-w-3xl leading-[30px]">
         I'm a skilled software engineer with experience in building highly performant, scalable and resilient applications. I am well versed with AWS and have extensive
