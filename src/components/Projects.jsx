a4caff2ee456944c2e4b9fc18b97d4b2386dd11c
Samrat Jha <samratj@amazon.com>
2023-09-16 01:23:31 -0400

    fix: Sigh

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,7 @@ const ProjectCard = ({ index, name, description, image, source_code_link, demo_l
         scale: 1,
         speed: 450
       }} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
-        <div className="relative w-full h-[230px] cursor-pointer" onClick={() => window.open(`${demo_link} ?? ${source_code_link}`, "_blank")}>
+        <div className="relative w-full h-[230px] cursor-pointer" onClick={() => window.open(`${demo_link ?? source_code_link}`, "_blank")}>
           <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div onClick={() => window.open(source_code_link, "_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
